87d136e13f39dbb1550c0988751ad3f3de1f196a
barokool <knabao7a7@gmail.com>
2023-06-13 18:03:05 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import ReactPlayer from "react-player";
+import axios from "axios";
+import { FileInput } from "@mantine/core";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
